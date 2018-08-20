2b5ecaae922bb39c55ffcf38f95da5c879859982
ricksmith26 <ricksmith26@hotmail.com>
2018-08-20 17:25:13 +0100

    mondayPm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import fs from 'fs';
+import GoogleLogin from 'react-google-login';
 
 import './App.css';
 
@@ -18,32 +18,56 @@ class App extends Component {
     // twitterPref: []
   };
   render() {
+    const responseGoogle = response => {
+      console.log(response);
+    };
     console.log(this.state);
     return (
       <div className="App">
+        <h1>Create your account</h1>
+        <br />
+
+        <p>Firstname: </p>
         <input type="text" onChange={this.handleFirstnameChange} />
+
         <br />
-        <br />
+        <p>Lastname: </p>
         <input type="text" onChange={this.handleLastnameChange} />
+
         <br />
-        <br />
+        <p>Screenname: </p>
         <input type="text" onChange={this.handleScreennameChange} />
+
         <br />
-        <br />
+        <h3>Work/School address </h3>
+        <p>Street No: </p>
         <input type="text" onChange={this.handleWorkStreetNoChange} />
+
         <br />
-        <br />
+        <p>Street: </p>
         <input type="text" onChange={this.handleWorkStreetChange} />
         <br />
         <br />
+        <p>City: : </p>
         <input type="text" onChange={this.handleWorkCityChange} />
+
         <br />
-        <br />
+        <p>Postcode: </p>
         <input type="text" onChange={this.handleWorkPostcodeChange} />
+
         <br />
-        <br />
+
+        <p>Add subjects to your news feed: </p>
+        {this.state.newsPref.forEach(function(n) {
+          return <p>{n}</p>;
+        })}
         <input type="text" onKeyUp={this.handleSubmitNews} />
-        <Button />
+
+        <br />
+        <br />
+        <button onClick={this.handleButtonSubmit}>Submit</button>
+        <button onClick={this.handleButtonPress}>press</button>
+        <button onCLick={() => {}} />
       </div>
     );
   }
@@ -69,15 +93,20 @@ class App extends Component {
     this.setState({ workPostcode: event.target.value });
   };
   handleSubmitNews = event => {
-    console.log(event);
     if (event.keyCode === 13) {
       console.log('hitENter<<<<<<<<<<<<<<<<<<<<,,,');
       this.setState({ newsPref: [...this.state.newsPref, event.target.value] });
     }
   };
-  handleButton = event => {
-    // fs.writeFile(`./UsersFiles/userdata.json`, JSON.stringify(this.state), (error)=> {error ? console.log(`write error: ${error.message}`) ? console.log(`Sucessfull write to ${this.state.firstname}`)})
-    fs.writeFile('./UsersFiles/userdata.json');
+  handleButtonSubmit = event => {
+    console.log('hit function');
+    localStorage.setItem(this.state.firstname, JSON.stringify(this.state));
+  };
+
+  handleButtonPress = event => {
+    console.log('hit button func');
+
+    console.log(JSON.parse(localStorage.Rick));
   };
 }
 
